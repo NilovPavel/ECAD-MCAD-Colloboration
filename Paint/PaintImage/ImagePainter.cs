db7e691b5af5ddb0b039c9e0ba89554d6f46d4f8
NilovPavel <nilowpavel@gmail.com>
2021-04-25 12:27:05 +0300

    Painter is work

--- Paint/PaintImage/ImagePainter.cs
+++ Paint/PaintImage/ImagePainter.cs
@@ -13,6 +13,7 @@ namespace PaintImage
         private Graphics grafics;
         //private GraphicsState graphicsState;
         private int scale;
+        private IEqualityComparer<Point> pointEqualityComparer;
 
         public void DrawArc(Color color, double axis1, double sweepAxis, double radius, double x0, double y0, double width)
         {
@@ -23,7 +24,7 @@ namespace PaintImage
             if (sweepAxis == 0)
                 return;
 
-            this.grafics.DrawArc(pen, (float)x0*this.scale, (float)y0 * this.scale, (float)radius * 2 * this.scale, (float)radius * 2 * this.scale, (float)axis1, (float)sweepAxis);
+            this.grafics.DrawArc(pen, (float)x0 * this.scale, (float)y0 * this.scale, (float)radius * 2 * this.scale, (float)radius * 2 * this.scale, (float)axis1, (float)sweepAxis);
         }
 
         public void DrawLine(Color color, double x1, double y1, double x2, double y2, double width)
@@ -51,62 +52,119 @@ namespace PaintImage
             SolidBrush brush = new SolidBrush(color);
             GraphicsPath path = new GraphicsPath();
 
-            //if(arcs.Count == 0 && lines.Count > 0)
+            if (arcs.Count == 0 && lines.Count > 0)
                 lines.ForEach(item => path.AddLine((float)item.Point1.X * this.scale, (float)item.Point1.Y * this.scale, (float)item.Point2.X * this.scale, (float)item.Point2.Y * this.scale));
 
-            //if(lines.Count == 0 && arcs.Count > 0)
-            arcs.ForEach(item => path.AddArc((float)(item.OriginalPoint.X - item.Radius) * this.scale, (float)(item.OriginalPoint.Y - item.Radius) * this.scale,
-                                                (float)(item.Radius * 2) * this.scale, (float)(item.Radius * 2) * this.scale, (float)item.StartAngle, (float)item.SweepAngle));
+            if (lines.Count == 0 && arcs.Count > 0)
+                arcs.ForEach(item => path.AddArc((float)(item.OriginalPoint.X - item.Radius) * this.scale, (float)(item.OriginalPoint.Y - item.Radius) * this.scale,
+                                                    (float)(item.Radius * 2) * this.scale, (float)(item.Radius * 2) * this.scale, (float)item.StartAngle, (float)item.SweepAngle));
 
-            /*if (lines.Count != 0 && arcs.Count != 0)
-                this.DrawPrimitivesByQueue(brush, path, lines, arcs);*/
+            if (lines.Count != 0 && arcs.Count != 0)
+                this.DrawPrimitivesByQueue(brush, path, lines, arcs);
 
 
             this.grafics.FillPath(brush, path);
         }
 
-        /*private void GetNextPrimitive(IPrimitive currentPrimitive)
-        {
-            IPrimitive nextPrimitive = arcs.Where(item => item.Point1.X == firstPrimitive.GetEndPoint().X && item.Point1.Y == firstPrimitive.GetEndPoint().Y).FirstOrDefault();
-            nextPrimitive = nextPrimitive ?? lines.Where(item => item.Point1.X == firstPrimitive.GetEndPoint().X && item.Point1.Y == firstPrimitive.GetEndPoint().Y).FirstOrDefault();
-            return nextPrimitive;
-        }*/
 
-        private void DrawPrimitivesByQueue(SolidBrush brush, GraphicsPath path, List<Line> lines, List<Arc> arcs)
+
+        /*private void DrawPrimitivesByQueue(SolidBrush brush, GraphicsPath path, List<Line> lines, List<Arc> arcs)
         {
-            /*IPrimitive firstPrimitive = lines[0];
+            IPrimitive firstPrimitive = lines[0];
             IPrimitive nextPrimitive = default(IPrimitive);
 
-
+            this.pointEqualityComparer.Equals()
             do
             {
-                nextPrimitive = arcs.Where(item => item.Point1.X == firstPrimitive.GetEndPoint().X && item.Point1.Y == firstPrimitive.GetEndPoint().Y).FirstOrDefault();
+                nextPrimitive = arcs.Where(item => item.Point1.X == firstPrimitive.GetStartPoint().X && item.Point1.Y == firstPrimitive.GetStartPoint().Y).FirstOrDefault();
                 if (nextPrimitive != null)
                 {
                     Arc nextArc = (Arc)nextPrimitive;
-                    if (nextPrimitive == null)
-                        return;
-                    path.AddArc((float)Math.Round(nextArc.OriginalPoint.X - nextArc.Radius, 2) * this.scale, (float)Math.Round(nextArc.OriginalPoint.Y - nextArc.Radius,2) * this.scale,
-                                                (float)(nextArc.Radius * 2) * this.scale, (float)(nextArc.Radius * 2) * this.scale, (float)nextArc.StartAngle, (float)nextArc.SweepAngle);
+                    path.AddArc((float)Math.Round(nextArc.OriginalPoint.X - nextArc.Radius, 1) * this.scale, (float)Math.Round(nextArc.OriginalPoint.Y - nextArc.Radius,1) * this.scale,
+                                                (float)(nextArc.Radius * 1) * this.scale, (float)(nextArc.Radius * 1) * this.scale, (float)nextArc.StartAngle, (float)nextArc.SweepAngle);
                 }
                 else
                 {
-                    nextPrimitive= lines.Where(item => Math.Round(item.Point1.X,2) == Math.Round(firstPrimitive.GetEndPoint().X,2) && Math.Round(item.Point1.Y,2) == Math.Round(firstPrimitive.GetEndPoint().Y,2)).FirstOrDefault();
-                    if (nextPrimitive == null)
-                        return;
+                    nextPrimitive= lines.Where(item => Math.Round(item.Point1.X,1) == Math.Round(firstPrimitive.GetEndPoint().X,1) && Math.Round(item.Point1.Y,1) == Math.Round(firstPrimitive.GetEndPoint().Y,1)).FirstOrDefault();
                     Line nextLine = (Line)nextPrimitive;
                     path.AddLine((float)nextLine.Point1.X * this.scale, (float)nextLine.Point1.Y * this.scale, (float)nextLine.Point2.X * this.scale, (float)nextLine.Point2.Y * this.scale);
                 }
                 firstPrimitive = nextPrimitive;
             }
-            while (nextPrimitive == lines[0]);*/
+            while (nextPrimitive == lines[0]);
 
+        }*/
+
+        private void DrawPrimitivesByQueue(SolidBrush brush, GraphicsPath path, List<Line> lines, List<Arc> arcs)
+        {
+            List<IPrimitive> primitiveQueue = this.GetPrimitivesQueue(lines, arcs);
+            primitiveQueue.Reverse();
+
+            for (int i = 0; i < primitiveQueue.Count; i++)
+            {
+                Arc currentArc = primitiveQueue[i] as Arc;
+                if(currentArc!=null)
+                    path.AddArc((float)(currentArc.OriginalPoint.X - currentArc.Radius) * this.scale, 
+                    (float)(currentArc.OriginalPoint.Y - currentArc.Radius) * this.scale,(float)(currentArc.Radius * 2) * this.scale,
+                    (float)(currentArc.Radius * 2) * this.scale, (float)currentArc.StartAngle, (float)currentArc.SweepAngle);
+
+                Line currentLine = primitiveQueue[i] as Line;
+                if(currentLine!=null)
+                    path.AddLine((float)currentLine.Point1.X * this.scale, (float)currentLine.Point1.Y * this.scale,
+                        (float)currentLine.Point2.X * this.scale, (float)currentLine.Point2.Y * this.scale);
+            }
+        }
+
+        private bool GetBools(IPrimitive firstPrimitive, IPrimitive secondPrimitive)
+        {
+            bool firstBool = this.pointEqualityComparer.Equals(firstPrimitive.GetEndPoint(), secondPrimitive.GetStartPoint());
+
+            bool secondBool = this.pointEqualityComparer.Equals(firstPrimitive.GetEndPoint(), secondPrimitive.GetEndPoint()) 
+                && !this.pointEqualityComparer.Equals(firstPrimitive.GetStartPoint(), secondPrimitive.GetStartPoint());
+
+            bool thirdBool = this.pointEqualityComparer.Equals(firstPrimitive.GetStartPoint(), secondPrimitive.GetStartPoint())
+                && !this.pointEqualityComparer.Equals(firstPrimitive.GetEndPoint(), secondPrimitive.GetEndPoint());
+
+            bool fourBool = this.pointEqualityComparer.Equals(firstPrimitive.GetStartPoint(), secondPrimitive.GetEndPoint());
+
+            bool result = firstBool || secondBool || thirdBool || fourBool;
+
+            return result;
+        }
+
+
+        private List<IPrimitive> GetPrimitivesQueue(List<Line> lines, List<Arc> arcs)
+        {
+            List<IPrimitive> destPrimitives = new List<IPrimitive>();
+            List<IPrimitive> sourcePrimitives = new List<IPrimitive>(lines);
+            sourcePrimitives.AddRange(arcs);
+
+            IPrimitive firstPrimitive = sourcePrimitives.First();
+            IPrimitive currentPrimitive = firstPrimitive;
+            IPrimitive nextPrimitive = default(IPrimitive);
+
+            do
+            {
+                destPrimitives.Add(currentPrimitive);
+                nextPrimitive = sourcePrimitives.Where(item => (this.GetBools(currentPrimitive, item)
+                                                                            && !destPrimitives.Exists(y => y.Equals(item))
+                                                                            )).FirstOrDefault();
+
+                currentPrimitive = nextPrimitive;
+                if (currentPrimitive == null)
+                    break;
+            }
+            while (!currentPrimitive.Equals(firstPrimitive));
+            
+            return destPrimitives;
         }
 
         public ImagePainter(Bitmap bitmap, int scale)
         {
             this.grafics = Graphics.FromImage(bitmap);
             this.scale = scale;
+
+            this.pointEqualityComparer = new PointEqualityComparer();
         }
 
         public void RestoreState()
---
+++ Paint/PaintImage/PointEqualityComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintImage
+{
+    class PointEqualityComparer : IEqualityComparer<Point>
+    {
+        public bool Equals(Point firstPoint, Point secondPoint)
+        {
+            return Math.Round(firstPoint.X, 2) == Math.Round(secondPoint.X, 2) && Math.Round(firstPoint.Y, 2) == Math.Round(secondPoint.Y, 2);
+        }
+
+        public int GetHashCode(Point obj)
+        {
+            return 0;
+        }
+    }
+}
